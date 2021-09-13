e9a6964b16b8f63febbedd41b06f1e41cf9ffb8b
ian <ianvinicioss@gmail.com>
2021-09-13 18:05:57 -0300

    Adding the sql script for database directorie

--- dataBase/conectDatabase.php
+++ dataBase/conectDatabase.php
@@ -2,7 +2,7 @@
   $host = 'localhost';
   $db = 'PRODUCT_LIST';
   $user = 'root';
-  $pass = 'Odeiobancodedados';
+  $pass = 'root';
   $dsn = "mysql:host=$host;dbname=$db;charset=utf8";
 
   $pdo = new PDO($dsn, $user, $pass);
